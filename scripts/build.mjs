e1418e59670a468930fdbef9743e06ce2d08ce54
wolfy1339 <4595477+wolfy1339@users.noreply.github.com>
2023-06-09 15:23:24 -0400

    fix(build): remove browser build
    This package is not meant for browser usage

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -35,27 +35,16 @@ async function main() {
 
   const entryPoints = ["./pkg/dist-src/index.js"];
 
-  await Promise.all([
-    // Build the a CJS Node.js bundle
-    esbuild.build({
-      entryPoints,
-      outdir: "pkg/dist-node",
-      bundle: true,
-      platform: "node",
-      target: "node14",
-      format: "cjs",
-      ...sharedOptions,
-    }),
-    // Build an ESM browser bundle
-    esbuild.build({
-      entryPoints,
-      outdir: "pkg/dist-web",
-      bundle: true,
-      platform: "browser",
-      format: "esm",
-      ...sharedOptions,
-    }),
-  ]);
+  // Build the a CJS Node.js bundle
+  await esbuild.build({
+    entryPoints,
+    outdir: "pkg/dist-node",
+    bundle: true,
+    platform: "node",
+    target: "node14",
+    format: "cjs",
+    ...sharedOptions,
+  });
 
   // Copy the README, LICENSE to the pkg folder
   await copyFile("LICENSE", "pkg/LICENSE");
@@ -75,7 +64,6 @@ async function main() {
         ...pkg,
         files: ["dist-*/**", "bin/**"],
         main: "dist-node/index.js",
-        browser: "dist-web/index.js",
         types: "dist-types/index.d.ts",
         module: "dist-src/index.js",
         sideEffects: false,
